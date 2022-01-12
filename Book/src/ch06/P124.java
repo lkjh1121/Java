ec5c96aedc2fa3f66210ad3bac2c7b75c4673eb1
unknown <akskejdtod@gmail.com>
2022-01-12 17:49:19 +0900

    2022/01/12 수업예제

--- Book/src/ch06/P124.java
+++ Book/src/ch06/P124.java
@@ -2,4 +2,20 @@ package ch06;
 
 public class P124 {
 
+	public static void main(String[] args) {
+	
+		// 원본 배열
+		int[] arrInt = {1,2,3}
+		
+		// 복사할 배열
+		int[] arrInt2 = new int [5];
+		
+		// 복사할 배열에 값 대입
+		for (int i=0; i<arrInt.length; i++) {
+			arrInt[i] = arrInt[i];
+		}
+		
+		
+	}
+
 }
